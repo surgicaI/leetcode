54b56cbe9a21a8d961da18789af1990117ae06ef
Simranjyot Singh Gill <simranjyot1991@gmail.com>
2017-06-05 01:06:23 -0400

    solution to binary-tree-postorder-traversal problem

---
+++ binary-tree-postorder-traversal.py
@@ -0,0 +1,24 @@
+# https://leetcode.com/problems/binary-tree-postorder-traversal/#/description
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def postorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        res = []
+        stack = [root]
+        while stack:
+            node = stack.pop()
+            if node:
+                res.append(node.val)
+                stack.append(node.left)
+                stack.append(node.right)
+        return res[::-1]
