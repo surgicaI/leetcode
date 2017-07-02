3303683bf0b386bd5d9ff40d9d3b914424e43a68
Simranjyot Singh Gill <simranjyot1991@gmail.com>
2017-07-02 19:37:48 -0400

    solution to bulb swithcer problem

---
+++ bulb-switcher.py
@@ -0,0 +1,10 @@
+# https://leetcode.com/problems/bulb-switcher/#/description
+
+
+class Solution(object):
+    def bulbSwitch(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        return int(n**(0.5))
